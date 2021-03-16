d83a877eccb432e279e59b81dad9a6658c7bf2ca
selviatunhasanah <selviatunhasanah1@gmail.com>
2021-03-16 16:10:32 +0700

    first commit

---
+++ kesalahan variable/src/kesalahan/variable/KesalahanVariable.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kesalahan.variable;
+
+/**
+ *
+ * @author selviatun hasanah
+ */
+public class KesalahanVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         //PENULISAN VARIABEL BENAR
+        boolean gameOver = false;
+        int student = 50, classes = 3;
+        double sales_tax;
+        short number1;
+
+        //PENULISAN VARIABEL SALAH 
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+    }
+    
+}
---
+++ person/src/person/student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Selviatun Hasanah
+ */
+class student {
+    String fName;
+    String stuId;
+    String stuStatus;
+}
